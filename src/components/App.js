8109e68008e6e967d0708e59e44eaf42d7676a75
Olanrewaju <lanrehnics@gmail.com>
2022-03-12 03:17:38 +0100

    complete dapp functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
+import Main from './Main';
 import Web3 from 'web3';
 import Tether from '../truffle_abis/Tether.json';
 import Rwd from '../truffle_abis/Rwd.json';
@@ -44,17 +45,16 @@ class App extends Component {
             window.alert('Error! Tether contract not deployed - no detected network')
         }
 
-         //Load Rwd Contract
-         const rwdData = Rwd.networks[networkId];
-         if (rwdData) {
-             const rwd = new web3.eth.Contract(Rwd.abi, rwdData.address);
-             this.setState({ rwd: rwd });
-             let rwdBalance = await rwd.methods.balanceOf(this.state.account).call();
-             this.setState({ rwdBalance: rwdBalance.toString() });
-             console.log(rwdBalance);
-         } else {
-             window.alert('Error! Reward contract not deployed - no detected network')
-         }
+        //Load Rwd Contract
+        const rwdData = Rwd.networks[networkId];
+        if (rwdData) {
+            const rwd = new web3.eth.Contract(Rwd.abi, rwdData.address);
+            this.setState({ rwd: rwd });
+            let rwdBalance = await rwd.methods.balanceOf(this.state.account).call();
+            this.setState({ rwdBalance: rwdBalance.toString() });
+        } else {
+            window.alert('Error! Reward contract not deployed - no detected network')
+        }
 
         //Load DecentralBank Contract
         const decentralBankData = DecentralBank.networks[networkId];
@@ -63,13 +63,29 @@ class App extends Component {
             this.setState({ decentralBank: decentralBank });
             let stakingBalance = await decentralBank.methods.stakingBalance(this.state.account).call();
             this.setState({ stakingBalance: stakingBalance.toString() });
-            console.log(stakingBalance);
         } else {
             window.alert('Error! DecentralBank contract not deployed - no detected network')
         }
 
-        this.setState({loading: false});
-       
+        this.setState({ loading: false });
+
+    }
+
+    stakeTokens = (amount) => {
+        this.setState({loading: true});
+        this.state.tether.methods.approve(this.state.decentralBank._address, amount).send({from: this.state.account}).on('transactionHash', (hash)=> {});
+        this.state.decentralBank.methods.depositTokens(amount).send({from: this.state.account}).on('transactionHash', (hash) => {
+            console.log(hash);
+            this.setState({loading: false});
+        });
+    }
+
+    unstakeTokens = () => {
+        this.setState({loading: true});
+        this.state.decentralBank.methods.unstakeTokens().send({from: this.state.account}).on('transactionHash', (hash) => {
+            console.log(hash);
+            this.setState({loading: false});
+        });
     }
 
     constructor(props) {
@@ -82,18 +98,37 @@ class App extends Component {
             tetherBalance: '0',
             rwdBalance: '0',
             stakingBalance: '0',
-            loading: true
+            loading: true,
+            stakeTokens : this.stakeTokens,
+            unstakeTokens : this.unstakeTokens
         }
     }
 
     render() {
+        let content
+        {
+            content = this.state.loading ? <p id='loader' className='text-center' style={{ margin: '30px' }} >LOADING PLEASE...</p> :
+                <Main
+                    tetherBalance={this.state.tetherBalance}
+                    rwdBalance={this.state.rwdBalance}
+                    stakingBalance={this.state.stakingBalance}
+                    stakeTokens={this.state.stakeTokens}
+                    unstakeTokens={this.state.unstakeTokens}
+                />
+        };
         return (
             <div>
                 <div>
                     <NavBar account={this.state.account} />
-                </div>
-                <div>
-                    <h1>Hello, World!</h1>
+                    <div className='container-fluid mt-5'>
+                        <div className='row'>
+                            <main role='main' className='col-lg-12 ml-auto mr-auto' style={{ maxWidth: '600px', minHeight: '100vm' }} >
+                                <div>
+                                    {content}
+                                </div>
+                            </main>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,7 +18,6 @@ class NavBar extends Component {
                 </ul>
             </nav>
         )
-
     }
 }
 
